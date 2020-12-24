cfb5d1ef76247435443b0725f08500cfc4615895
Manos Liakos <manosliakoss@gmail.com>
2020-12-25 00:12:30 +0200

    Added user avatar icon

--- src/components/FeatureTitle/FeatureTitle.js
+++ src/components/FeatureTitle/FeatureTitle.js
@@ -3,6 +3,8 @@ import React, { useState, useEffect } from "react";
 const FeatureTitle = props => {
 	const [title, setTitle] = useState("");
 
+	// TODO: use useDebounce here too
+
 	useEffect(() => {
 		(async () => {
 			const res = await fetch(
--- src/components/Users/User/User.js
+++ src/components/Users/User/User.js
@@ -8,13 +8,14 @@ const User = props => {
 	return (
 		<div className="flex space-x-3">
 			<img
-				className="h-6 w-6 rounded-full"
-				src="https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=3&w=256&h=256&q=80"
-				alt=""
+				className="h-8 w-8
+				 rounded-full"
+				src="https://i.ibb.co/FggrjYN/user-solid.png"
+				alt="avatar"
 			/>
 			<div className="flex-1 space-y-1">
 				<div className="flex items-center justify-between">
-					<h3 className="text-sm font-medium">{props.name}</h3>
+					<h3 className="text-md font-medium p-2">{props.name}</h3>
 					<p className="text-sm text-gray-500">
 						{state.hidden ? hiddenVote : shownVote}
 					</p>
