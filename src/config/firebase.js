dc93e27840421420919027d13df019331cf0ddd6
Manos Leaks <manosliakoss@gmail.com>
2022-05-26 15:58:06 +0300

    chore: Adds .env file

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,14 +4,14 @@ import "firebase/analytics";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-	apiKey: "AIzaSyDugZBp5jARwwqtb-NVJo5zlXe7IYY6LOY",
+	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
 	authDomain: "scrum-voting-app-7fa10.firebaseapp.com",
 	databaseURL: "https://scrum-voting-app-7fa10-default-rtdb.firebaseio.com",
 	projectId: "scrum-voting-app-7fa10",
 	storageBucket: "scrum-voting-app-7fa10.appspot.com",
 	messagingSenderId: "21017286243",
-	appId: "1:21017286243:web:811a26bf4629514fb90a56",
-	measurementId: "G-T63DD0E6R3"
+	appId: process.env.REACT_APP_FIREBASE_APP_ID,
+	measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
